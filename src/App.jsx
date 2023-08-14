25947710ad33360f4da3f508cf8f5172c910db94
Khushal Agarwal <starkhush5@gmail.com>
2023-08-14 11:39:06 +0530

    app init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1>SeaLog</h1>
+      <h1>Currently in Developement</h1>
+    </>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+import vitePluginPrettier from "vite-plugin-prettier";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint(), vitePluginPrettier()],
+});
